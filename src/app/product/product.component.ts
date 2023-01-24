dda1b38f35ac2ec05715cdf5a3cb4a5e11165a60
MuskanBh <muskanbharalawala10@gmail.com>
2023-01-24 15:57:48 +0530

    Add Product Basic Feature

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,13 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'in-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductComponent implements OnInit {
+  productForm: FormGroup;
+  @Input() product;
+  edit= false;
+  deviceType='tablet';
+  deviceTypes=[{
+    name: 'Tablet',
+    icon:'tablet',
+  },{
+    name:'Laptop',
+    icon:'computer',
+  },{
+    name:'Phone',
+    icon:'mobile',
+  },{
+    name: 'Monitor',
+    icon: 'display',
+  }]
+  selectDevice(device){
+    this.deviceType= device.icon;
+  }
+  get basicFeatures(): FormArray {
+    return this.productForm.get('basic.features') as FormArray;
+    }
+  addFeature() {
+    this.basicFeatures.push(this.fb.control(''));
+  }
+  constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      basic:fb.group({
+        name: '',
+        description:'',
+        active: false,
+        features: fb.array([
+          fb.control('')
+        ])
+      }),
+      expiration: fb.group({
+        expirationDate: null
+      })
+    });
+  }
+  handleClose(){
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
